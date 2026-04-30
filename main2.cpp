7e6927be09f262c932e3b92c179416ba878af13b
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-30 19:41:23 -0300

    commit two

---
+++ main2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+int main(){
+
+    int choice;
+    float temp, converted;
+
+    std::cout<<"------ Temperature Converter ------"<<std::endl;
+    std::cout<<"1: Celsius to Fahrenheit"<<std::endl;
+    std::cout<<"2: Fahrenheit to Celsius"<<std::endl;
+    std::cout<<"Choose an option (1 or 2):"<<std::endl;
+    std::cin>>choice;
+
+    if(choice == 1){
+        std::cout<<"Enter temperature in Celsius"<<std::endl;
+        std::cin>>temp;
+        converted = (temp * 1.8) + 32;
+        std::cout<<temp<<"C os "<<converted<<" F."<<std::endl;
+    }else if(choice == 2){
+        std::cout<<"Enter temperature in Fahrenheit: "<<std::endl;
+        std::cin>>temp;
+        converted = (temp - 32) / 1.8;
+        std::cout<<temp<<"F is "<<converted<<" C."<<std::endl;
+    }else{
+        std::cout<<"Invalid Option! Please restart."<<std::endl;
+    }
+
+    return 0;
+
+}
