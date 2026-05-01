2095c4f64f54e65e096ca78d58a7cc63ce4efe6a
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-30 21:03:26 -0300

    commit 5

---
+++ main5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+
+    int totalSeconds;
+
+    std::cout<<"Enter total seconds: "<<std::endl;
+    std::cin>>totalSeconds;
+
+    int minutes = totalSeconds / 60;
+    int seconds = totalSeconds % 60;
+
+    std::cout<<"Time: "<<minutes<<" minutes and "<<seconds<<" seconds."<<std::endl;
+
+    return 0;
+}
